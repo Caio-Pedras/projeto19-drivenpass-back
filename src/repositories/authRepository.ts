5329aef4a0c9fdc2a621f34d19a61dd672337a16
CaioPedras <caio.vp@gmail.com>
2022-09-12 15:40:06 -0300

    prepare deploy

--- src/repositories/authRepository.ts
+++ src/repositories/authRepository.ts
@@ -2,7 +2,7 @@ import { prisma } from "./../config/database.js";
 import { CreateUserData } from "../services/authService.js";
 
 export async function findUserByEmail(email: string) {
-  return public.prisma.user.findUnique({ where: { email } });
+  return prisma.public.user.findUnique({ where: { email } });
 }
 export async function insertUser(user: CreateUserData) {
   return prisma.user.create({ data: user });
